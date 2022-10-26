cc80c3d4a7331d2f8df8de8ab37f6adf5935eed0
penpe <penpa@bc.fi>
2022-10-26 14:07:41 +0300

    Change Post method

--- src/TodoApi/Controllers/TodoController.cs
+++ src/TodoApi/Controllers/TodoController.cs
@@ -9,6 +9,8 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
+
+
     [Route("api/[controller]")]
     [ApiController]
     public class TodoController : ControllerBase
@@ -85,14 +87,11 @@ namespace TodoApi.Controllers
         [HttpPost]
         public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
         {
-          if (_context.TodoItems == null)
-          {
-              return Problem("Entity set 'TodoContext.TodoItems'  is null.");
-          }
             _context.TodoItems.Add(todoItem);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
         }
 
         // DELETE: api/Todo/5
